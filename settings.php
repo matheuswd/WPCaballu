9bdfb30443913c7bbeeae38a03d2387f7cdf0801
Matheus Martins <matheusdealmeidamartins@gmail.com>
2016-10-31 12:07:39 -0200

    adding new taxonomy and fixing cpt

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 
 // Register Custom Post Type
-function wpcaballu() {
+function wpcaballu_cpt() {
 
 	$labels = array(
 		'name'                  => _x( 'Horses', 'Post Type General Name', 'wpcaballu' ),
@@ -35,7 +35,7 @@ function wpcaballu() {
 		'description'           => __( 'The post type where you can create your horses and it\'s genealogy', 'wpcaballu' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'thumbnail', ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
+		'taxonomies'            => array( 'wpcaballu_taxonomy' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
@@ -47,9 +47,48 @@ function wpcaballu() {
 		'has_archive'           => true,		
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
+		'capability_type'       => 'post',
 	);
-	register_post_type( 'wpcaballu', $args );
+	register_post_type( 'wpcaballu_cpt', $args );
 
 }
-add_action( 'init', 'wpcaballu', 0 );
+add_action( 'init', 'wpcaballu_cpt', 0 );
+
+// Register Custom Taxonomy
+function wpcaballu_horse_breeds() {
+
+	$labels = array(
+		'name'                       => _x( 'Horse Breeds', 'Taxonomy General Name', 'wpcaballu' ),
+		'singular_name'              => _x( 'Horse Breed', 'Taxonomy Singular Name', 'wpcaballu' ),
+		'menu_name'                  => __( 'Breed', 'wpcaballu' ),
+		'all_items'                  => __( 'All Breeds', 'wpcaballu' ),
+		'parent_item'                => __( 'Parent Item', 'wpcaballu' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'wpcaballu' ),
+		'new_item_name'              => __( 'New Breed', 'wpcaballu' ),
+		'add_new_item'               => __( 'Add New Breed', 'wpcaballu' ),
+		'edit_item'                  => __( 'Edit Breed', 'wpcaballu' ),
+		'update_item'                => __( 'Update Breed', 'wpcaballu' ),
+		'view_item'                  => __( 'View Breed', 'wpcaballu' ),
+		'separate_items_with_commas' => __( 'Separate breeds with commas', 'wpcaballu' ),
+		'add_or_remove_items'        => __( 'Add or remove breeds', 'wpcaballu' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'wpcaballu' ),
+		'popular_items'              => __( 'Popular Breeds', 'wpcaballu' ),
+		'search_items'               => __( 'Search Breeds', 'wpcaballu' ),
+		'not_found'                  => __( 'Not Found', 'wpcaballu' ),
+		'no_terms'                   => __( 'No breeds', 'wpcaballu' ),
+		'items_list'                 => __( 'Breeds list', 'wpcaballu' ),
+		'items_list_navigation'      => __( 'Breeds list navigation', 'wpcaballu' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'wpcaballu_taxonomy', array( 'wpcaballu_cpt' ), $args );
+
+}
+add_action( 'init', 'wpcaballu_horse_breeds', 0 );
